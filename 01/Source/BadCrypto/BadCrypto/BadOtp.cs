76c2c732db07d96a82213e0617f520147c8e6864
Steven Gann <sgann2012@fau.edu>
2020-03-16 20:46:57 -0700

    Puzzle 01 - Trivial RNG and bad OTP

---
+++ 01/Source/BadCrypto/BadCrypto/BadOtp.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BadCrypto
+{
+    internal class BadOtp
+    {
+        public byte[] Pad = new byte[4096];
+        public int EncryptIndex = 0;
+        public int DecryptIndex = 0;
+
+        public BadOtp()
+        {
+            Initialize(0, 0);
+        }
+
+        public BadOtp(int Seed, int Index)
+        {
+            Initialize(Seed, Index);
+        }
+
+        public string Encrypt(string Plaintext)
+        {
+            char[] charsBuffer = Plaintext.ToCharArray();
+            byte[] buffer = new byte[charsBuffer.Length];
+#if DEBUG
+            Console.WriteLine();
+#endif
+            for (int i = 0; i < charsBuffer.Length; i++)
+            {
+                try
+                {
+                    buffer[i] = (byte)(Convert.ToByte(charsBuffer[i]) ^ Pad[EncryptIndex++]);
+                }
+                catch
+                {
+                    //Unicode. Ugh. Who needs it?
+                    buffer[i] = 0;
+                    EncryptIndex++;
+                }
+#if DEBUG
+                Console.Write(buffer[i] + " ");
+#endif
+                if (EncryptIndex >= Pad.Length) { EncryptIndex = 0; }
+            }
+
+            return Convert.ToBase64String(buffer);
+        }
+
+        public string Decrypt(string Cyphertext)
+        {
+            byte[] buffer = Convert.FromBase64String(Cyphertext);
+            char[] charsBuffer = new char[buffer.Length];
+#if DEBUG
+            Console.WriteLine();
+#endif
+            for (int i = 0; i < charsBuffer.Length; i++)
+            {
+                charsBuffer[i] = Convert.ToChar((byte)(buffer[i] ^ Pad[DecryptIndex++]));
+#if DEBUG
+                Console.Write(buffer[i] + " ");
+#endif
+                if (DecryptIndex >= Pad.Length) { DecryptIndex = 0; }
+            }
+
+            return new string(charsBuffer);
+        }
+
+        public void Initialize(int Seed, int Index)
+        {
+            EncryptIndex = Index;
+            DecryptIndex = Index;
+            BadRandom RNG = new BadRandom(Seed);
+
+#if DEBUG
+            int[] counts = new int[256];
+            Console.Write("Generating OTP\n\t");
+#endif
+            for (int i = 0; i < Pad.Length; i++)
+            {
+                byte b = RNG.NextByte();
+                Pad[i] = b;
+
+#if DEBUG
+                counts[b]++;
+                Console.Write(b.ToString() + "\t");
+#endif
+            }
+#if DEBUG
+            Console.Write("\n");
+            Console.Write("OTP Distribution\n\t");
+            for (int i = 0; i < 256; i++)
+            {
+                Console.Write(counts[i] + "\t");
+            }
+#endif
+        }
+    }
+}
---
+++ 01/Source/BadCrypto/BadCrypto/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BadCrypto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BadOtp otp = new BadOtp();
+
+            if (args.Length > 0)
+            {
+                if (args[0].ToLower().Contains(".txt"))
+                {
+                    if(args.Length >= 2)
+                    {
+                        try
+                        {
+                            int seed = int.Parse(args[1]);
+                            otp = new BadOtp(seed, 0);
+                        }
+                        catch
+                        {
+                            Console.WriteLine("\nFailed to parse OTP seed! Using default.");
+                            Console.ReadLine();
+                        }
+                    }
+
+                    if (args[0].ToLower().Contains("encrypted"))
+                    {
+                        Console.WriteLine("Decrypting file...");
+                        System.IO.StreamReader inputFile = new System.IO.StreamReader(args[0]);
+                        System.IO.StreamWriter outFile = new System.IO.StreamWriter("decrypted." + Convert.ToString(DateTime.UtcNow.Subtract(new DateTime(1970, 1, 1)).TotalSeconds) + ".txt");
+                        string line;
+                        while ((line = inputFile.ReadLine()) != null)
+                        {
+                            Console.Write(".");
+                            string plaintext = otp.Decrypt(line);
+                            outFile.WriteLine(plaintext);
+                        }
+
+                        inputFile.Close();
+                        outFile.Close();
+                    }
+                    else
+                    {
+                        Console.WriteLine("Encrypting file...");
+                        Console.Write(".");
+                        System.IO.StreamReader inputFile = new System.IO.StreamReader(args[0]);
+                        System.IO.StreamWriter outFile = new System.IO.StreamWriter("encrypted." + Convert.ToString(DateTime.UtcNow.Subtract(new DateTime(1970, 1, 1)).TotalSeconds) + ".txt");
+                        string line;
+                        while ((line = inputFile.ReadLine()) != null)
+                        {
+                            string ciphertext = otp.Encrypt(line);
+                            outFile.WriteLine(ciphertext);
+                        }
+
+                        inputFile.Close();
+                        outFile.Close();
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Unsupported argument");
+                }
+            }
+            else
+            {
+                Console.Write("\nInteractive Mode");
+                Console.WriteLine("\n====================================================");
+                Console.WriteLine("Enter some text to send securely!");
+                while (true)
+                {
+                    string ciphertext = otp.Encrypt(Console.ReadLine());
+                    string plaintext = otp.Decrypt(ciphertext);
+                    Console.WriteLine("\n====================================================");
+                    Console.Write("Encrypted:\n" + ciphertext);
+                    Console.WriteLine("\n----------------------------------------------------");
+                    Console.Write("Decrypted:\n" + plaintext);
+                    Console.WriteLine("\n====================================================");
+                }
+            }
+        }
+    }
+}
